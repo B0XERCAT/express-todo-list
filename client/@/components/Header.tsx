50ac2b663faee6f5ed12bdc61434a5aca3cc0940
b0xercat <seojin3154@naver.com>
2024-01-26 18:53:13 +0900

    feat(fe): add link component to header

--- client/@/components/Header.tsx
+++ client/@/components/Header.tsx
@@ -1,7 +1,8 @@
+import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <div className="w-full bg-blue-500 text-white text-center py-4 font-bold text-3xl">
-      TODO List
+      <Link to="/">TODO List</Link>
     </div>
   );
 }
